c3f53c4c3f3673d9b01581d155a17fd55793bfd6
fainnerramirez <fainnerramirez@gmail.com>
2025-11-25 12:38:11 -0500

    feat: instalación de material UI

---
+++ src/pages/chat.page.tsx
@@ -0,0 +1,13 @@
+import { Box, Button } from "@mui/material";
+
+const ChatPage = () => {
+    return (
+        <Box>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </Box>
+    )
+}
+
+export default ChatPage;
