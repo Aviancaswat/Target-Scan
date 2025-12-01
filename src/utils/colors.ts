d2a383e99762407d9fded0456bcb439e828b6208
fainnerramirez <fainnerramirez@gmail.com>
2025-12-01 11:56:13 -0500

    feat: se cambia estilos del mensaje del usuario

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,24 +1,16 @@
-import {
-    amber,
-    blue,
-    blueGrey,
-    cyan,
-    deepOrange,
-    deepPurple,
-    green,
-    teal
-} from "@mui/material/colors";
+import { theme } from "./theme";
 
 export const getColor = () => {
     const colorOptions: [string, string][] = [
-        [blueGrey[100], blueGrey[700]],
-        [blue[100], blue[600]],
-        [cyan[100], cyan[700]],
-        [green[100], green[700]],
-        [teal[100], teal[700]],
-        [deepPurple[100], deepPurple[600]],
-        [amber[100], amber[700]],
-        [deepOrange[100], deepOrange[700]],
+        // [blueGrey[100], blueGrey[700]],
+        // [blue[100], blue[600]],
+        // [cyan[100], cyan[700]],
+        // [green[100], green[700]],
+        // [teal[100], teal[700]],
+        // [deepPurple[100], deepPurple[600]],
+        // [amber[100], amber[700]],
+        // [deepOrange[100], deepOrange[700]],
+        [theme.palette.primary.main, theme.palette.text.primary]
     ];
 
     const randomIndex = Math.floor(Math.random() * colorOptions.length);
