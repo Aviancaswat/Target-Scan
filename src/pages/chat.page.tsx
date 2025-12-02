c279c2fab28c6d8a3374ffb4c3f59315901795c3
fainnerramirez <fainnerramirez@gmail.com>
2025-12-02 10:10:39 -0500

    feat: se agregan mensaje en chat page historial

--- src/pages/chat.page.tsx
+++ src/pages/chat.page.tsx
@@ -1,15 +1,19 @@
 import { Box, Icon, IconButton } from "@mui/material";
+import { doc, onSnapshot } from "firebase/firestore";
 import "highlight.js/styles/tokyo-night-dark.css";
 import { Check, Copy, type LucideIcon } from "lucide-react";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
+import { v4 as uuid } from "uuid";
 import Navbar from "../components/chat/navbar";
 import { ChatInput } from "../components/chat/textAreaCustom";
 import WelcomeChat from "../components/chat/welcome-chat";
+import { conversationCollection } from "../firebase/firestore/collections/conversation.collection";
 import { AgentTargetScanService } from "../services/targetScanService";
+import { useTargetScanStore, type ConversationsTargetScan } from "../store/target-store";
 import '../styles/chat.style.css';
 import { getColor } from "../utils/colors";
 
@@ -19,6 +23,15 @@ type Message = {
 };
 
 const ChatPage = () => {
+
+    const {
+        currentConversationId: conversationId,
+        currentMessages: messages,
+        setCurrentConversationId: setConversationId,
+        setCurrentMessages: setMessages,
+        setConversations,
+        conversations
+    } = useTargetScanStore();
 
     const [question, setQuestion] = useState<string>("");
     const [messages, setMessages] = useState<Message[]>([]);
@@ -114,6 +127,52 @@ const ChatPage = () => {
             messagesEndRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
         }
     }, [messages]);
+
+    useEffect(() => {
+        const conversationUUID = uuid();
+        setConversationId(conversationUUID);
+        console.log("Se creo la conversación con id: ", conversationUUID);
+        console.log("conversationsAPA: ", conversationUUID);
+    }, [])
+
+    useEffect(() => {
+        console.log("Se actualiza las conversaciones: ", conversations)
+    }, [conversations])
+
+    useEffect(() => {
+
+        if (!conversationId) return;
+
+        const ref = doc(conversationCollection, conversationId);
+
+        const unsub = onSnapshot(ref, (snap) => {
+            const data = snap.data();
+
+            if (!data) return;
+
+            const conversation: ConversationsTargetScan = {
+                converdationId: conversationId,
+                title: data.title ?? "Nuevo chat",
+                messages: data.messages ?? [],
+            };
+
+            setMessages(conversation.messages);
+
+            setConversations((prev) => {
+                const exists = prev.some(c => c.converdationId === conversationId);
+
+                if (!exists) {
+                    return [...prev, conversation];
+                }
+
+                return prev.map(c =>
+                    c.converdationId === conversationId ? conversation : c
+                );
+            });
+        });
+
+        return () => unsub();
+    }, [conversationId]);
 
     return (
         <Box
